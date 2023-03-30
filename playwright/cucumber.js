8b60ce2d061db873c99e08bca5af0c46b2dcdb0b
michoo-wan <michoowan@gmail.com>
2023-03-31 00:29:00 +0200

    comment about cucumber-js

--- playwright/cucumber.js
+++ playwright/cucumber.js
@@ -1,3 +1,8 @@
+/*
+cucumber-js was prepared by copying maunal from this blog (works as of 31.03.2023):
+https://www.indiumsoftware.com/blog/automation-using-playwright-library-with-cucumber-js/
+*/
+
 let options = [
   '--require-module ts-node/register',
   '--require ./steps/*.steps.ts',
